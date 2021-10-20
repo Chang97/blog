e2ca1fc1ab51b2e9f42a2d2b6ba35aecd00a4915
Chang97 <ckdrbsaos@naver.com>
2021-10-20 23:57:23 +0900

    boardDelete

--- blog/src/main/java/com/cos/blog/service/BoardService.java
+++ blog/src/main/java/com/cos/blog/service/BoardService.java
@@ -1,9 +1,9 @@
 package com.cos.blog.service;
 
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 //스프링이 컴포넌트 스캔을 통해서 Bean에 등록을 해줌. IoC를 해준다.
 import org.springframework.transaction.annotation.Transactional;
@@ -23,9 +23,23 @@ public class BoardService {
 		board.setUser(user);
 		boardRepository.save(board);
 	}
+	
+	@Transactional(readOnly = true)
+	public Page<Board> 글목록(Pageable pageable) {
+		return boardRepository.findAll(pageable);
+	}
+	
+	@Transactional(readOnly = true)
+	public Board 글상세보기(int id) {
+		return boardRepository.findById(id)
+				.orElseThrow(()->{
+					return new IllegalArgumentException("글 상세보기 실패 : 아이디를 찾을 수 없습니다.");
+				});
+	}
 
-	public List<Board> 글목록() {
-		return boardRepository.findAll();
+	@Transactional
+	public void 삭제(int id) {
+		boardRepository.deleteById(id);
 	}
 	
 	
